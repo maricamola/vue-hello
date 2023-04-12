5f9fab462995e37f4e875382bbdeb8d577553198
maricamola <maricamola98@libero.it>
2023-04-12 17:58:34 +0200

    image&button added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,19 @@ const {createApp} = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      image: "https://images2.corriereobjects.it/methode_image/2017/12/14/Tecnologia/Foto%20Tecnologia/3oKIPsx2VAYAgEHC12.gif?v=201712151212"
     }
+  },
+
+  methods: {
+
+    click(){
+      console.log("Hai cliccato!")
+    }
+
   }
+
 }).mount("#app")
 
 
